5104ce8d1856e8608eda1e51cd98aba30b6ae5ff
Guan Jiannan <guanjiannan@outlook.com>
2020-10-14 17:05:29 +0800

    [暂存] 第三章 树 练习一

--- Chapter-3-Tree/Practice/Chapter-3-Tree-Practice-1/header.h
+++ Chapter-3-Tree/Practice/Chapter-3-Tree-Practice-1/header.h
@@ -1,7 +1,7 @@
 #pragma once
 
 
-/* threaded_binary_tree.cpp */
+/* tb_tree.cpp */
 
 /**
  * \brief 树节点储存的数据类型
@@ -11,105 +11,121 @@ typedef char element_type;
 /**
  * \brief 树节点
  */
-struct tree_node
+struct node
 {
     element_type data;
 
-    struct tree_node* left;
-    bool is_left_child;
+    struct node* l_child;
+    bool l;
 
-    struct tree_node* right;
-    bool is_right_child;
+    struct node* r_child;
+    bool r;
 };
 
 /**
  * \brief 树类型
  */
-typedef struct tree_node* tree;
+typedef struct node* tree;
 
 /**
  * \brief 计算二叉树节点个数
- * \param binary_tree 二叉树
+ * \param b_tree 二叉树
  * \return 返回节点个数
  */
-int count_node(tree binary_tree);
+int size(tree b_tree);
 
 /**
  * \brief 获取二叉树高度
- * \param binary_tree 二叉树
+ * \param b_tree 二叉树
  * \return 返回高度
  */
-int count_height(tree binary_tree);
+int height(tree b_tree);
 
 /**
  * \brief 释放二叉树内存
- * \param binary_tree 二叉树
+ * \param b_tree 二叉树
  */
-void destroy(tree binary_tree);
+void destroy(tree b_tree);
 
 /**
  * \brief 交换二叉树的所有节点子树（镜像对称）
- * \param binary_tree 二叉树
+ * \param b_tree 二叉树
  */
-void get_mirror(tree binary_tree);
+void mirror(tree b_tree);
 
 /**
  * \brief 接受用户输入的二叉树
  * \return 返回输入的二叉树
  */
-tree input_binary_tree();
+tree input();
 
 /**
  * \brief 按照线索获取指定节点的后继节点
- * \param binary_tree 线索二叉树
+ * \param tb_tree 线索二叉树
  * \param node 指定节点
  * \return 返回后继节点
  */
-tree_node* get_threaded_next(tree binary_tree, tree_node* node);
+node* tb_next(tree tb_tree, node* node);
 
 /**
  * \brief 初始化线索
  */
-void init_threaded_binary_tree();
+void init();
 
 /**
  * \brief 线索二叉树先序遍历
- * \param binary_tree 线索二叉树
+ * \param tb_tree 线索二叉树
  */
-void pre_order_travel(tree binary_tree);
+void pre_order(tree tb_tree);
 
 /**
  * \brief 线索二叉树中序遍历
- * \param binary_tree 线索二叉树
+ * \param tb_tree 线索二叉树
  */
-void in_order_travel(tree binary_tree);
+void in_order(tree tb_tree);
+
+/**
+ * \brief 层序遍历二叉树，遍历结果保存到数组中
+ * \param b_tree 二叉树
+ * \param list 遍历结果数组
+ * \param index 遍历结果数组下标
+ */
+void layer(tree b_tree, node** list, int& index);
 
 /**
  * \brief 在线索二叉树指定节点后插入节点
- * \param binary_tree 线索二叉树
- * \param insert_position 插入位置
- * \param insert_node 待插入的节点
+ * \param tb_tree 线索二叉树
+ * \param position 插入位置
+ * \param node 待插入的节点
  */
-void insert_node(tree binary_tree, tree_node* insert_position, tree_node* insert_node);
+void insert(tree tb_tree, node* position, node* node);
+
+/**
+ * \brief 将层序遍历数组转换为二叉树
+ * \param list 层序遍历数组
+ * \param len 层序遍历数组长度
+ * \return 返回转换后的二叉树
+ */
+tree list_to_tree(node** list, int len);
 
 /**
  * \brief 复制二叉树
- * \param source_tree 源二叉树
+ * \param source 源二叉树
  * \return 返回副本二叉树
  */
-tree copy(tree source_tree);
+tree copy(tree source);
 
 /**
  * \brief 递归复制二叉树
- * \param source_tree 源二叉树
+ * \param source 源二叉树
  * \return 返回副本二叉树
  */
-tree copy_recurse(tree source_tree);
+tree copy_r(tree source);
 
 /**
  * \brief 判断二叉树是否相同
- * \param binary_tree_1 第一个二叉树
- * \param binary_tree_2 第二个二叉树
+ * \param b_tree_1 第一个二叉树
+ * \param b_tree_2 第二个二叉树
  * \return 返回是否相同的结果
  */
-bool is_equal(tree binary_tree_1, tree binary_tree_2);
+bool equal(tree b_tree_1, tree b_tree_2);
---
+++ Chapter-3-Tree/Practice/Chapter-3-Tree-Practice-1/tb_tree.cpp
@@ -0,0 +1,69 @@
+﻿#include "header.h"
+
+int size(tree b_tree)
+{
+    return 0;
+}
+
+int height(tree b_tree)
+{
+    return 0;
+}
+
+void destroy(tree b_tree)
+{
+}
+
+void mirror(tree b_tree)
+{
+}
+
+tree input()
+{
+    return nullptr;
+}
+
+node* tb_next(tree tb_tree, node* node)
+{
+    return nullptr;
+}
+
+void init()
+{
+}
+
+void pre_order(tree tb_tree)
+{
+}
+
+void in_order(tree tb_tree)
+{
+}
+
+void layer(tree b_tree, node** list, int& index)
+{
+}
+
+void insert(tree tb_tree, node* position, node* node)
+{
+}
+
+tree list_to_tree(node** list, int len)
+{
+    return nullptr;
+}
+
+tree copy(tree source)
+{
+    return nullptr;
+}
+
+tree copy_r(tree source)
+{
+    return nullptr;
+}
+
+bool equal(tree b_tree_1, tree b_tree_2)
+{
+    return false;
+}
